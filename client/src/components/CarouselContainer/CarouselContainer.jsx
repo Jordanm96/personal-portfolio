0dabf0d9753d1503c5c9fc339ce9ca475e899298
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-05-04 15:34:28 -0700

    Carousel looks decent.

--- client/src/components/CarouselContainer/CarouselContainer.jsx
+++ client/src/components/CarouselContainer/CarouselContainer.jsx
@@ -1,55 +1,47 @@
 import Carousel from "react-bootstrap/Carousel";
-import './CarouselContainer.css';
-import { Link } from 'react-router-dom';
+import "./CarouselContainer.css";
+import { Link } from "react-router-dom";
 
 const CarouselContainer = () => {
   return (
     <div>
       <h4>Click one!</h4>
-      <Carousel id='carousel'>
-        <Carousel.Item>
-          <Link to='/fresh'>
-          <img
-            className="d-block w-100"
-            src="https://i.imgur.com/PzMIvjU.png"
-            alt="First slide"
+      <Carousel id="carousel" indicators={false}>
+        <Carousel.Item interval={900}>
+          <Link to="/fresh">
+            <img
+              className="d-block w-100"
+              src="https://i.imgur.com/PzMIvjU.png"
+              alt="First slide"
             />
+          <Carousel.Caption>
+            <h4>FRESH TOMATOES</h4>
+          </Carousel.Caption>
           </Link>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <Link to='ima-miru'>
-          <img
-            className="d-block w-100"
-            src="https://i.imgur.com/pXD4D0f.png"
-            alt="Second slide"
+        <Carousel.Item interval={900}>
+          <Link to="ima-miru">
+            <img
+              className="d-block w-100"
+              src="https://i.imgur.com/pXD4D0f.png"
+              alt="Second slide"
             />
-            </Link>
-
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h4>IMA MIRU</h4>
           </Carousel.Caption>
+          </Link>
         </Carousel.Item>
-        <Carousel.Item>
-        <Link to='group-run'>
-
-          <img
-            className="d-block w-100"
-            src="https://i.imgur.com/cQXUbQw.png"
-            alt="Third slide"
-          />
-            </Link>
-
+        <Carousel.Item interval={900}>
+          <Link to="group-run">
+            <img
+              className="d-block w-100"
+              src="https://i.imgur.com/cQXUbQw.png"
+              alt="Third slide"
+            />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h4>GROUP RUN!</h4>
           </Carousel.Caption>
+          </Link>
         </Carousel.Item>
       </Carousel>
     </div>
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,12 +1,15 @@
 import CarouselContainer from "../../components/CarouselContainer/CarouselContainer";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./Home.css";
 const Home = () => {
   return (
     <>
       <nav>
-        <h1> <a href="#home">Jordan</a></h1>
+        <h1>
+          {" "}
+          <a href="#home">Jordan</a>
+        </h1>
         <ul>
           <li>
             <a href="#about">About Me</a>
@@ -19,51 +22,60 @@ const Home = () => {
           </li>
         </ul>
       </nav>
-      <div className='body'>
-
-      <section id="home">
-        <h2>Welcome,</h2>
-        <h2>My name is Jordan Anthony Montero.</h2>
-        <h2>Nice to meet you!</h2>
-      </section>
-      <section id="about">
-        <h3>About JAM</h3>
-        <img src="https://i.imgur.com/Ey5DhH3.jpg" alt="jordan" />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </section>
-      <section id="projects">
-        <h3>Projects</h3>
-        <CarouselContainer />
-        {/* Insert carousel component here with 3 images
-      clickable links to each project */}
-      </section>
-      <section id="contact">
-        <h3>Contact Me!</h3>
-        <div className="contact-container">
-          <div className="contact-info">
-            <h4>Contact Info</h4>
-            <img src="https://i.imgur.com/Ey5DhH3.jpg" alt="jordan" />
-            <p>Phone: 916-698-0989</p>
-            <p>Email: jordmontero96@gmail.com</p>
-            <p>Location: San Diego, CA</p>
+      <div className="body">
+        <section id="home">
+          <h2>Welcome,</h2>
+          <h2>My name is Jordan Anthony Montero.</h2>
+          <h2>Nice to meet you!</h2>
+        </section>
+        <section id="about">
+          <h3>About JAM</h3>
+          <img src="https://i.imgur.com/Ey5DhH3.jpg" alt="jordan" />
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            {/* RIGHT NOW THIS IS ONLY LINKING TO GOOGLE
+            IT NEEDS TO LINK TO A PDF OF MY RESUME */}
+            <a
+              className="resume-link"
+              href="google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Resumé
+            </a>
           </div>
-          <div className="contact-form">
-            <h4>Let's work together!</h4>
-            <input placeholder="Name" />
-            <input placeholder="Email" />
-            <input placeholder="Message" />
-            <button>Send</button>
+        </section>
+        <section id="projects">
+          <h3>Projects</h3>
+          <CarouselContainer />
+        </section>
+        <section id="contact">
+          <h3>Contact Me!</h3>
+          <div className="contact-container">
+            <div className="contact-info">
+              <h4>Contact Info</h4>
+              <img src="https://i.imgur.com/Ey5DhH3.jpg" alt="jordan" />
+              <p>Phone: 916-698-0989</p>
+              <p>Email: jordmontero96@gmail.com</p>
+              <p>Location: San Diego, CA</p>
+            </div>
+            <div className="contact-form">
+              <h4>Let's work together!</h4>
+              <input placeholder="Name" />
+              <input placeholder="Email" />
+              <input placeholder="Message" />
+              <button>Send</button>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
       </div>
     </>
   );

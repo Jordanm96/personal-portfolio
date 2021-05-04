4091b9d8b82acf3198fb76b7e8dbfdd316622eab
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-05-03 19:55:37 -0700

    Set up the basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./screens/Home/Home";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <>
+      <nav>
+        <h1>Jordan</h1>
+        <ul className="nav">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <section id="home">
+        <h2>Welcome,</h2>
+        <h2>My name is Jordan Anthony Montero.</h2>
+        <h2>Nice to meet you!</h2>
+      </section>
+      <section id="about">
+        <h3>About JAM</h3>
+        <img src="https://i.imgur.com/Ey5DhH3.jpg" alt="jordan" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
+      <section id="projects">
+        <h3>Projects</h3>
+        {/* Insert carousel component here with 3 images
+      clickable links to each project */}
+      </section>
+      <section id="contact">
+        <h3>Contact Me!</h3>
+        <div className="contact-container">
+          <div className="contact-info">
+            <h4>Contact Info</h4>
+            <img src="https://i.imgur.com/Ey5DhH3.jpg" alt="jordan" />
+            <p>Phone: 916-698-0989</p>
+            <p>Email: jordmontero96@gmail.com</p>
+            <p>Location: San Diego, CA</p>
+          </div>
+          <div className="contact-form">
+            <h4>Let's work together!</h4>
+            <input placeholder="Name" />
+            <input placeholder="Email" />
+            <input placeholder="Message" />
+            <button>Send</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;

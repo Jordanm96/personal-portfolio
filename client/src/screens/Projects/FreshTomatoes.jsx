5df96dbb45e391179f15fea96451d926c51ee4cc
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-05-05 14:21:20 -0700

    Added details to each project screen

--- client/src/screens/Projects/FreshTomatoes.jsx
+++ client/src/screens/Projects/FreshTomatoes.jsx
@@ -5,7 +5,21 @@ const ProjectDetail = () => {
   return (
     <div>
       <DetailNav />
+      <img src="https://i.imgur.com/PzMIvjU.png" alt="Fresh Tomatoes" />
       <h4>Fresh Tomatoes</h4>
+      <p>
+        <strong>Description:</strong> Fresh Tomatoes (inspired by rotten
+        tomatoes) is an application that allows users to access a list of movies
+        with reviews written by the users themselves. People who access this app
+        will create an account so that they may view and add movies they have
+        watched. Creating an account will also allow users to leave reviews on
+        movies they have watched. Fresh Tomatoes lets the user become a
+        movie critic, as they write their own personal opinions on the movie and
+        leave it with a rating to help other users decide what to watch next.
+      </p>
+      <p>
+        <strong>Built Using:</strong>{' '}REACT, JS, CSS, POSTGRESQL, RUBY ON RAILS
+      </p>
     </div>
   );
 };
--- client/src/screens/Projects/ImaMiru.jsx
+++ client/src/screens/Projects/ImaMiru.jsx
@@ -5,7 +5,20 @@ const ImaMiru = () => {
   return (
     <div>
       <DetailNav />
+      <img src="https://i.imgur.com/pXD4D0f.png" alt="Ima Miru" />
       <h4>Ima Miru</h4>
+      <p>
+        <strong>Description:</strong> Ima Miru (which translates to "watch now"
+        in Japanese) is an application where users can browse various anime
+        shows and their corresponding descriptions. Users who sign up are able
+        to add new shows to the current list on the main page. Users may also
+        save shows of interest in a section called Watchlist. The Watchlist
+        varies per user and lists out any show that user has saved to watch
+        later.
+      </p>
+      <p>
+        <strong>Built Using:</strong>{' '}React, JS, CSS, Express, MongoDB
+      </p>
     </div>
   );
 };
